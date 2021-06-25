3335d47fcbcc4f41e7c8f90f53886dbbd4a81838
Gowry Sugathan <38757089+gowry189@users.noreply.github.com>
2021-06-25 15:07:07 +0530

    Update constructor.c

--- constructor.c
+++ constructor.c
@@ -16,6 +16,13 @@ namespace ConsoleApp1
             empname = "sam";
             sal = 8000;
         }
+        public con_class(int empno, string empname, int sal)
+        {
+            this.empno = empno;
+            this.empname = empname;
+            this.sal = sal;
+        }
+
         public void printValues()
         {
             Console.WriteLine("empno = {0} empname = {1} salary = {2}", empno,empname,sal);
@@ -26,7 +33,9 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             con_class con = new con_class();
+            con_class con1 = new con_class(101,"Gowry",5000);
             con.printValues();
+            con1.printValues();
         }
     }
 }
