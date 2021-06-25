ccd1fed046bebed52ecf7274d42880710d3b81d6
Gowry Sugathan <38757089+gowry189@users.noreply.github.com>
2021-06-25 15:23:09 +0530

    Update constructor.c

--- constructor.c
+++ constructor.c
@@ -16,6 +16,25 @@ namespace ConsoleApp1
             empname = "sam";
             sal = 8000;
         }
+        public con_class(int empno)
+        {
+            this.empno = empno;
+            empname = "sam";
+            sal = 8000;
+        }
+        public con_class(con_class obj)
+        {
+            this.empno = obj.empno;
+            this.empname = obj.empname;
+            this.sal = obj.sal;
+        }
+
+        public con_class(string empname)
+        {
+            this.empno = 104;
+            this.empname = empname;
+           this.sal = 8000;
+        }
         public con_class(int empno, string empname, int sal)
         {
             this.empno = empno;
@@ -33,9 +52,15 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             con_class con = new con_class();
-            con_class con1 = new con_class(101,"Gowry",5000);
             con.printValues();
+            con_class con1 = new con_class(101,"Gowry",5000);
             con1.printValues();
+            con_class con2 = new con_class(102);
+            con2.printValues();
+            con_class con3 = new con_class("Akshay");
+            con3.printValues();
+            con_class con4 = new con_class(con2);
+            con4.printValues();
         }
     }
 }
